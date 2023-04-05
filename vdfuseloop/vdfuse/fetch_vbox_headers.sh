fcfd3e4b6650a22eb08a6798d3e7a0f0b65e5492
Geng Zhang <3215497340@qq.com>
2023-04-05 15:00:11 +0800

    Delete vdfuseloop/vdfuse directory

--- vdfuseloop/vdfuse/fetch_vbox_headers.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )" 
-# grab per revision, eg @81014
-# check with https://www.virtualbox.org/browser/vbox/trunk be careful about
-# bleeding edge versions!
-svn co http://www.virtualbox.org/svn/vbox/trunk/include@81014 ${SCRIPT_DIR}/include/
